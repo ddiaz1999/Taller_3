3d54204fd3cf815d01630c7704a8c25b214c1583
JhonHader <haderfh@gmail.com>
2020-09-13 12:37:24 -0500

    all filters

--- filters.py
+++ filters.py
@@ -1,18 +1,61 @@
+''' Ruler 1         2         3         4         5         6         7        '
+/*******************************************************************************
+*                                                                              *
+*          /$$$$$$   /$$  /$$    /$$                                           *
+*         /$$__  $$ |__/ | $$   | $$                                           *
+*        | $$  \__/  /$$ | $$  /$$$$$$     /$$$$$$    /$$$$$$    /$$$$$$$      *
+*        | $$$$     | $$ | $$ |_  $$_/    /$$__  $$  /$$__  $$  /$$_____/      *
+*        | $$_/     | $$ | $$   | $$     | $$$$$$$$ | $$  \__/ |  $$$$$$       *
+*        | $$       | $$ | $$   | $$ /$$ | $$_____/ | $$        \____  $$      *
+*        | $$       | $$ | $$   |  $$$$/ |  $$$$$$$ | $$        /$$$$$$$/      *
+*        |__/       |__/ |__/    \___/    \_______/ |__/       |_______/       *
+*                                                                              *
+*                                                                              *
+*                  Developed by:                                               *
+*                                                                              *
+*                            Jhon Hader Fernandez                              *
+*                     - jhon_fernandez@javeriana.edu.co                        *
+*                                                                              *
+*                             Diego Fernando Diaz                              *
+*                        - di-diego@javeriana.edu.co                           *
+*                                                                              *                                                                              *
+*                       Pontificia Universidad Javeriana                       *
+*                            Bogota DC - Colombia                              *
+*                                  Sep - 2020                                  *
+*                                                                              *
+*****************************************************************************'''
+
+#------------------------------------------------------------------------------#
+#                          IMPORT MODULES AND LIBRARIES                        #
+#------------------------------------------------------------------------------#
+
 import cv2
 
+
+#------------------------------------------------------------------------------#
+#                                  FILTER CLASS                                #
+#------------------------------------------------------------------------------#
+
 class filter:
-    def __init__(self,image_noisy):
-        self.noisy = image_noisy
+    def __init__(self, image_noisy):
+        self.__image_noisy = image_noisy
 
-    def filter_type(self,filter_name):
-        #if filter_name == '':
+    def filter_type(self, filter_type):
+        if filter_type == 'gaussian':
+            kernel_shape = (7, 7)
+            sigma = 1.5
+            gaussian_filtered = blur = cv2.GaussianBlur(self.__image_noisy, kernel_shape, sigma, sigma)
+            return gaussian_filtered
 
-        #elif filter_name == '':
+        elif filter_type == 'median':
+            kernel_size = 7
+            median_filtered = cv2.medianBlur(self.__image_noisy, kernel_size)
+            return median_filtered
 
-        if filter_name == 'bilateral':
-            image_bilateral = cv2.bilateralFilter(self.noisy, 15, 25, 25)
-            return image_bilateral
+        if filter_type == 'bilateral':
+            bilateral_filtered = cv2.bilateralFilter(self.__image_noisy, 15, 25, 25)
+            return bilateral_filtered
 
-        elif filter_name == 'nlm':
-            image_nlm = cv2.fastNlMeansDenoising(self.noisy, 5, 15, 25)
-            return image_nlm
+        elif filter_type == 'nlm':
+            nlm_filtered = cv2.fastNlMeansDenoising(self.__image_noisy, 5, 15, 25)
+            return nlm_filtered
--- main.py
+++ main.py
@@ -1,34 +1,88 @@
+''' Ruler 1         2         3         4         5         6         7        '
+/*******************************************************************************
+*                                                                              *
+*                                           /$$                                *
+*                                          |__/                                *
+*                 /$$$$$$/$$$$    /$$$$$$   /$$  /$$$$$$$                      *
+*                | $$_  $$_  $$  |____  $$ | $$ | $$__  $$                     *
+*                | $$ \ $$ \ $$   /$$$$$$$ | $$ | $$  \ $$                     *
+*                | $$ | $$ | $$  /$$__  $$ | $$ | $$  | $$                     *
+*                | $$ | $$ | $$ |  $$$$$$$ | $$ | $$  | $$                     *
+*                |__/ |__/ |__/  \_______/ |__/ |__/  |__/                     *
+*                                                                              *
+*                  Developed by:                                               *
+*                                                                              *
+*                            Jhon Hader Fernandez                              *
+*                     - jhon_fernandez@javeriana.edu.co                        *
+*                                                                              *
+*                             Diego Fernando Diaz                              *
+*                        - di-diego@javeriana.edu.co                           *
+*                                                                              *                                                                              *
+*                       Pontificia Universidad Javeriana                       *
+*                            Bogota DC - Colombia                              *
+*                                  Sep - 2020                                  *
+*                                                                              *
+*****************************************************************************'''
+
+#------------------------------------------------------------------------------#
+#                          IMPORT MODULES AND LIBRARIES                        #
+#------------------------------------------------------------------------------#
+
 from noise import *
 from filters import *
-import os
+
+#------------------------------------------------------------------------------#
+#                                   FUNCTIONS                                  #
+#------------------------------------------------------------------------------#
+
+def subplot_image(image1, image2, label1, label2):
+    bg_color = [0, 0, 0]
+    font = cv2.FONT_HERSHEY_COMPLEX
+    image1_padding = cv2.copyMakeBorder(image1.copy(), 50, 5, 5, 5, cv2.BORDER_CONSTANT, value=bg_color)
+    image2_padding = cv2.copyMakeBorder(image2.copy(), 50, 5, 5, 5, cv2.BORDER_CONSTANT, value=bg_color)
+    image1_padding = cv2.putText(image1_padding.copy(), label1, (int(0.25 * image1_padding.shape[0]), 30), font, 1, [255, 0, 0])
+    image2_padding = cv2.putText(image2_padding.copy(), label2, (int(0.25 * image1_padding.shape[0]), 30), font, 1, [255, 0, 0])
+    image = cv2.hconcat((image1_padding, image2_padding))
+    return image
+
+
+#------------------------------------------------------------------------------#
+#                                     MAIN                                     #
+#------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
-    #C:\Users\di-di\OneDrive\Escritorio\imagenes_vision\lena.jpg
-    ruta_imagen_lena = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision\lena.jpg'
-    image_lena = cv2.imread(ruta_imagen_lena,1)
+
+    path_file = r'C:\Users\lenovo\Desktop\JHON_2030\PROCESAMIENTO_DE_IMAGENES\Talleres\Semana_6\Taller\lena.png'
+    #path_file = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision\lena.jpg'
+    image_lena = cv2.imread(path_file, 1)
     image_lena_gray = cv2.cvtColor(image_lena, cv2.COLOR_BGR2GRAY)
     image_lena_noisy = noise_generator(image_lena_gray.astype(np.float) / 255)
-    lena_gauss_noisy = image_lena_noisy.noise('gauss')
+    lena_gaussian_noisy = image_lena_noisy.noise('gauss')
     lena_sp_noisy = image_lena_noisy.noise('s&p')
-    lena_gauss_noisy = (255 * lena_gauss_noisy).astype(np.uint8)
+    lena_gaussian_noisy = (255 * lena_gaussian_noisy).astype(np.uint8)
     lena_sp_noisy = (255 * lena_sp_noisy).astype(np.uint8)
-    cv2.imshow('ruido gaussiano',lena_gauss_noisy)
-    cv2.imshow('ruido s&p',lena_sp_noisy)
+    cv2.imshow('Lena noises', subplot_image(lena_gaussian_noisy, lena_sp_noisy, 'Gaussian Noise', 'Salt-peper Noise'))
 
-
-    ##FILTRADO DE RUIDO GAUSSIANO
-    lena_gauss_noisy_filtered = filter(lena_gauss_noisy)
-    lena_gauss_noisy_filtered_bilateral = lena_gauss_noisy_filtered.filter_type('bilateral')
-    lena_gauss_noisy_filtered_nlm = lena_gauss_noisy_filtered.filter_type('nlm')
-    cv2.imshow('bilateral',lena_gauss_noisy_filtered_bilateral)
-    cv2.imshow('nlm', lena_gauss_noisy_filtered_nlm)
+    #FILTRADO DE RUIDO GAUSSIANO
+    lena_gaussian_noisy_filtered = filter(lena_gaussian_noisy)
+    lena_gaussian_noisy_filtered_gaussian = lena_gaussian_noisy_filtered.filter_type('gaussian')
+    lena_gaussian_noisy_filtered_median = lena_gaussian_noisy_filtered.filter_type('median')
+    lena_gaussian_noisy_filtered_bilateral = lena_gaussian_noisy_filtered.filter_type('bilateral')
+    lena_gaussian_noisy_filtered_nlm = lena_gaussian_noisy_filtered.filter_type('nlm')
+    cv2.imshow('Gaussian Noise - Gaussian Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_gaussian, 'Gaussian Noise', 'Gaussian Filter'))
+    cv2.imshow('Gaussian Noise - Median Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_median, 'Gaussian Noise', 'Median Filter'))
+    cv2.imshow('Gaussian Noise - Bilateral Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_bilateral, 'Gaussian Noise', 'Bilateral Filter'))
+    cv2.imshow('Gaussian Noise - NLM Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_nlm, 'Gaussian Noise', 'NLM Filter'))
 
     #FILTRADO A RUIDO S&P
     lena_sp_noisy_filtered = filter(lena_sp_noisy)
+    lena_sp_noisy_filtered_gaussian = lena_sp_noisy_filtered.filter_type('gaussian')
+    lena_sp_noisy_filtered_median = lena_sp_noisy_filtered.filter_type('median')
     lena_sp_noisy_filtered_bilateral = lena_sp_noisy_filtered.filter_type('bilateral')
     lena_sp_noisy_filtered_nlm = lena_sp_noisy_filtered.filter_type('nlm')
-    cv2.imshow('bilateral', lena_sp_noisy_filtered_bilateral)
-    cv2.imshow('nlm', lena_sp_noisy_filtered_nlm)
-
+    cv2.imshow('S&P Noise - Gaussian Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_gaussian, 'S&P Noise', 'Gaussian Filter'))
+    cv2.imshow('S&P Noise - Median Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_median, 'S&P Noise', 'Median Filter'))
+    cv2.imshow('S&P Noise - Bilateral Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_bilateral, 'S&P Noise', 'Bilateral Filter'))
+    cv2.imshow('S&P Noise - NLM Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_nlm, 'S&P Noise', 'NLM Filter'))
 
     cv2.waitKey(0)
