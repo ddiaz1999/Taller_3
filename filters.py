755fe1627f654380394d2a721265c0e4f7c3a861
JhonHader <haderfh@gmail.com>
2020-09-13 13:56:56 -0500

    time

--- filters.py
+++ filters.py
@@ -30,6 +30,7 @@
 #------------------------------------------------------------------------------#
 
 import cv2
+from time import time
 
 #------------------------------------------------------------------------------#
 #                                  FILTER CLASS                                #
@@ -39,28 +40,45 @@ class filter:
     def __init__(self, image_noisy):
         self.__image_noisy = image_noisy
 
+    def __Gaussian_filter(self):
+        kernel_shape = (7, 7)
+        sigma = 1.5
+        gaussian_filtered = blur = cv2.GaussianBlur(self.__image_noisy, kernel_shape, sigma, sigma)
+        return gaussian_filtered
+
+    def __Median_filter(self):
+        kernel_size = 7
+        median_filtered = cv2.medianBlur(self.__image_noisy, kernel_size)
+        return median_filtered
+
+    def __Bilateral_filter(self):
+        d = 15
+        sigmaColor = 25
+        sigmaSpace = 25
+        bilateral_filtered = cv2.bilateralFilter(self.__image_noisy, d, sigmaColor, sigmaSpace)
+        return bilateral_filtered
+
+    def __NLM_filter(self):
+        h = 5
+        windowSize = 15
+        searchSize = 25
+        start_time = time()
+        nlm_filtered = cv2.fastNlMeansDenoising(self.__image_noisy, h, windowSize, searchSize)
+        return nlm_filtered
+
+    def __time_execution(self, function_filter):
+        start_time = time()
+        function_return = function_filter()
+        elapsed_time = time() - start_time
+        return function_return, elapsed_time
+
     def filter_type(self, filter_type):
         if filter_type == 'gaussian':
-            kernel_shape = (7, 7)
-            sigma = 1.5
-            gaussian_filtered = blur = cv2.GaussianBlur(self.__image_noisy, kernel_shape, sigma, sigma)
-            return gaussian_filtered
-
+            Filter, time = self.__time_execution(self.__Gaussian_filter)
         elif filter_type == 'median':
-            kernel_size = 7
-            median_filtered = cv2.medianBlur(self.__image_noisy, kernel_size)
-            return median_filtered
-
+            Filter, time =  self.__time_execution(self.__Median_filter)
         elif filter_type == 'bilateral':
-            d = 15
-            sigmaColor = 25
-            sigmaSpace = 25
-            bilateral_filtered = cv2.bilateralFilter(self.__image_noisy, d, sigmaColor, sigmaSpace)
-            return bilateral_filtered
-
+            Filter, time = self.__time_execution(self.__Bilateral_filter)
         elif filter_type == 'nlm':
-            h = 5
-            windowSize = 15
-            searchSize = 25
-            nlm_filtered = cv2.fastNlMeansDenoising(self.__image_noisy, h, windowSize, searchSize)
-            return nlm_filtered
+            Filter, time = self.__time_execution(self.__NLM_filter)
+        return Filter, time
--- main.py
+++ main.py
@@ -30,6 +30,8 @@
 
 from noise import *
 from filters import *
+from decimal import Decimal
+from prettytable import PrettyTable
 
 #------------------------------------------------------------------------------#
 #                                   FUNCTIONS                                  #
@@ -45,6 +47,17 @@ def subplot_image(image1, image2, label1, label2):
     image = cv2.hconcat((image1_padding, image2_padding))
     return image
 
+def show_times(time, labels, title):
+    time_table = PrettyTable(["Filter", "Execution time (s)"])
+    for i, t in enumerate(time):
+        time[i] = '%.2E' % Decimal(str(t))
+    time_table.add_row([labels[0], time[0]])
+    time_table.add_row([labels[1], time[1]])
+    time_table.add_row([labels[2], time[2]])
+    time_table.add_row([labels[3], time[3]])
+    print('\n Execution time: filters to '+ title)
+    print(time_table, '\n')
+
 
 #------------------------------------------------------------------------------#
 #                                     MAIN                                     #
@@ -52,8 +65,8 @@ def subplot_image(image1, image2, label1, label2):
 
 if __name__ == '__main__':
 
-    #path_file = r'C:\Users\lenovo\Desktop\JHON_2030\PROCESAMIENTO_DE_IMAGENES\Talleres\Semana_6\Taller\lena.png'
-    path_file = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision\lena.jpg'
+    path_file = r'C:\Users\lenovo\Desktop\JHON_2030\PROCESAMIENTO_DE_IMAGENES\Talleres\Semana_6\Taller\lena.png'
+    #path_file = r'C:\Users\di-di\OneDrive\Escritorio\imagenes_vision\lena.jpg'
     image_lena = cv2.imread(path_file, 1)
     image_lena_gray = cv2.cvtColor(image_lena, cv2.COLOR_BGR2GRAY)
     image_lena_noisy = noise_generator(image_lena_gray.astype(np.float) / 255)
@@ -65,24 +78,28 @@ if __name__ == '__main__':
 
     #FILTRADO DE RUIDO GAUSSIANO
     lena_gaussian_noisy_filtered = filter(lena_gaussian_noisy)
-    lena_gaussian_noisy_filtered_gaussian = lena_gaussian_noisy_filtered.filter_type('gaussian')
-    lena_gaussian_noisy_filtered_median = lena_gaussian_noisy_filtered.filter_type('median')
-    lena_gaussian_noisy_filtered_bilateral = lena_gaussian_noisy_filtered.filter_type('bilateral')
-    lena_gaussian_noisy_filtered_nlm = lena_gaussian_noisy_filtered.filter_type('nlm')
-    cv2.imshow('Gaussian Noise - Gaussian Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_gaussian, 'Gaussian Noise', 'Gaussian Filter'))
-    cv2.imshow('Gaussian Noise - Median Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_median, 'Gaussian Noise', 'Median Filter'))
-    cv2.imshow('Gaussian Noise - Bilateral Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_bilateral, 'Gaussian Noise', 'Bilateral Filter'))
-    cv2.imshow('Gaussian Noise - NLM Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_nlm, 'Gaussian Noise', 'NLM Filter'))
+    lena_gaussian_noisy_filtered_gaussian, Gn_gaussian_time = lena_gaussian_noisy_filtered.filter_type('gaussian')
+    lena_gaussian_noisy_filtered_median, Gn_median_time = lena_gaussian_noisy_filtered.filter_type('median')
+    lena_gaussian_noisy_filtered_bilateral, Gn_bilateral_time = lena_gaussian_noisy_filtered.filter_type('bilateral')
+    lena_gaussian_noisy_filtered_nlm, Gn_nlm_time = lena_gaussian_noisy_filtered.filter_type('nlm')
+    #cv2.imshow('Gaussian Noise - Gaussian Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_gaussian, 'Gaussian Noise', 'Gaussian Filter'))
+    #cv2.imshow('Gaussian Noise - Median Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_median, 'Gaussian Noise', 'Median Filter'))
+    #cv2.imshow('Gaussian Noise - Bilateral Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_bilateral, 'Gaussian Noise', 'Bilateral Filter'))
+    #cv2.imshow('Gaussian Noise - NLM Filter', subplot_image(lena_gaussian_noisy, lena_gaussian_noisy_filtered_nlm, 'Gaussian Noise', 'NLM Filter'))
 
-    #FILTRADO A RUIDO S&P
-    lena_sp_noisy_filtered = filter(lena_sp_noisy)
-    lena_sp_noisy_filtered_gaussian = lena_sp_noisy_filtered.filter_type('gaussian')
-    lena_sp_noisy_filtered_median = lena_sp_noisy_filtered.filter_type('median')
-    lena_sp_noisy_filtered_bilateral = lena_sp_noisy_filtered.filter_type('bilateral')
-    lena_sp_noisy_filtered_nlm = lena_sp_noisy_filtered.filter_type('nlm')
-    cv2.imshow('S&P Noise - Gaussian Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_gaussian, 'S&P Noise', 'Gaussian Filter'))
-    cv2.imshow('S&P Noise - Median Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_median, 'S&P Noise', 'Median Filter'))
-    cv2.imshow('S&P Noise - Bilateral Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_bilateral, 'S&P Noise', 'Bilateral Filter'))
-    cv2.imshow('S&P Noise - NLM Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_nlm, 'S&P Noise', 'NLM Filter'))
+    show_times([Gn_gaussian_time, Gn_median_time, Gn_bilateral_time, Gn_nlm_time], ['Gaussian', 'Median', 'Bilateral', 'NLM'], 'Gaussian Noise')
+
+    # #FILTRADO A RUIDO S&P
+    # lena_sp_noisy_filtered = filter(lena_sp_noisy)
+    # lena_sp_noisy_filtered_gaussian, SPn_gaussian_time = lena_sp_noisy_filtered.filter_type('gaussian')
+    # lena_sp_noisy_filtered_median, SPn_median_time = lena_sp_noisy_filtered.filter_type('median')
+    # lena_sp_noisy_filtered_bilateral, SPn_bilateral_time  = lena_sp_noisy_filtered.filter_type('bilateral')
+    # lena_sp_noisy_filtered_nlm, SPn_bilateral_time = lena_sp_noisy_filtered.filter_type('nlm')
+    # cv2.imshow('S&P Noise - Gaussian Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_gaussian, 'S&P Noise', 'Gaussian Filter'))
+    # cv2.imshow('S&P Noise - Median Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_median, 'S&P Noise', 'Median Filter'))
+    # cv2.imshow('S&P Noise - Bilateral Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_bilateral, 'S&P Noise', 'Bilateral Filter'))
+    # cv2.imshow('S&P Noise - NLM Filter', subplot_image(lena_sp_noisy, lena_sp_noisy_filtered_nlm, 'S&P Noise', 'NLM Filter'))
+
+    show_times([Gn_gaussian_time, Gn_median_time, Gn_bilateral_time, Gn_nlm_time], ['Gaussian', 'Median', 'Bilateral', 'NLM'], 'Gaussian Noise')
 
     cv2.waitKey(0)
